cbab1bf4a70596761214288e8be778e21214f9d7
Nik Graf <nik@nikgraf.com>
2018-10-28 19:59:17 +0100

    improve styles and add info

--- site/src/pages/index.js
+++ site/src/pages/index.js
@@ -13,7 +13,7 @@ function compare(hookA, hookB) {
 const sortedHooks = hooks.sort(compare);
 
 const Hook = styled.div`
-  margin-bottom: 4rem;
+  margin-bottom: 3rem;
 `;
 
 const RepositoryLink = styled.a`
@@ -35,7 +35,8 @@ const Tag = styled.span`
 
 const FilterInput = styled.input`
   width: 100%;
-  margin-bottom: 2rem;
+  margin-top: 1rem;
+  margin-bottom: 3rem;
   padding: 0.4rem 0.8rem;
   font-family: "Roboto", sans-serif;
 `;
@@ -46,10 +47,17 @@ class IndexPage extends React.Component {
   render() {
     return (
       <Layout>
-        <p>
+        <p style={{ marginTop: '3rem' }}>
           Warning: Hooks are currently a React
           <a href={"https://github.com/reactjs/rfcs/pull/68"}> RFC</a> and{" "}
           <strong>not ready for production</strong>.
+        </p>
+        <p>
+          You can add your hooks by opening a pull-request at{" "}
+          <a href={"https://github.com/nikgraf/react-hooks"}>
+            https://github.com/nikgraf/react-hooks
+          </a>
+          .
         </p>
         <FilterInput
           value={this.state.term}
